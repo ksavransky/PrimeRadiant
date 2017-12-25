ced9b03208073010228b92943e4e7056164930e0
ksavransky <ksavransky@gmail.com>
2017-12-25 11:18:40 -0800

    rid of old code

--- server/api.js
+++ server/api.js
@@ -1,8 +1,6 @@
 const api = module.exports = require('express').Router()
 
 var Twitter = require('twitter-node-client').Twitter;
-var MarkovChain = require('markovchain');
-// var MarkovChain = require('markovchain-generate');
  
  
 function twitterSearch(req, res){
@@ -26,44 +24,8 @@ function twitterSearch(req, res){
   twitter.getUserTimeline({ screen_name: req.query.userName, count: '10'}, error, success);
 }
 
-function generatePhrase(req, res){
-  console.log('in generatePhrase');
-  console.log(req.query.text)
-  var success = function (phrase) {
-    // console.log('Data [%s]', data);
-    res.status(200).send(phrase);
-  };
-  
-  // markovchain-generate library code:
-  // var chain = new MarkovChain()
-  // chain.generateChain(req.query.text);
-  // var phrase = chain.generateString();
-  
-  // markovchain libraray code:
-  var chain = new MarkovChain(req.query.text);
-  console.log('chain')
-  console.log(chain)
-  
-  var getRandomWord = function(wordBank) {
-    if(!wordBank || wordBank === {}) {
-      return ''
-    }
-    var wordsArray = Object.keys(wordBank);
-    return wordsArray[Math.floor(Math.random() * wordsArray.length)];
-  }
-  
-  var phrase = chain.start(getRandomWord(chain.wordBank)).process();
-  
-  
-  
-  console.log('phrase')
-  console.log(phrase)
-  success(phrase);
-}
-
 api
   .get('/twitter-search', twitterSearch) 
-  .get('/generate-phrase', generatePhrase) 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
 
